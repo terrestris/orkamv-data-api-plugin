65d525798629bd1ecf3bf9b29f08f57ab5e8b42c
Simon Seyock <seyock@terrestris.de>
2021-05-11 11:48:07 +0200

    translate everything

---
+++ i18n/de.ts
@@ -0,0 +1,114 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="2.0">
+<context>
+    <name>OrkamvDataApiPlugin</name>
+    <message>
+        <location filename="../orkamv_data_api_plugin.py" line="199"/>
+        <source>&amp;ORKa.MV Data API</source>
+        <translation>&amp;ORKa.MV Data API</translation>
+    </message>
+    <message>
+        <location filename="../orkamv_data_api_plugin.py" line="187"/>
+        <source>ORKa.MV Data API</source>
+        <translation>ORKa.MV Data API</translation>
+    </message>
+    <message>
+        <location filename="../orkamv_data_api_plugin.py" line="259"/>
+        <source>WARNING: No SVG folder available! Please check your preferences.</source>
+        <translation>WARNUNG: Kein SVG Verzeichnus verfügbar! Bitte überprüfen Sie ihre Einstellungen.</translation>
+    </message>
+    <message>
+        <location filename="../orkamv_data_api_plugin.py" line="270"/>
+        <source>The directory is not empty</source>
+        <translation>Das Verzeichnis ist nicht leer</translation>
+    </message>
+    <message>
+        <location filename="../orkamv_data_api_plugin.py" line="271"/>
+        <source>Do you want to overwrite the content?</source>
+        <translation>Möchten Sie den Inhalt überschreiben?</translation>
+    </message>
+    <message>
+        <location filename="../orkamv_data_api_plugin.py" line="388"/>
+        <source>An error occured. Please try again layer and contact an administrator if the error still occurs.</source>
+        <translation>Ein Fehler ist aufgetreten. Bitte versuchen Sie es später noch einmal und kontaktieren Sie den Adminsitrator, wenn der Fehler immer noch auftritt.</translation>
+    </message>
+    <message>
+        <location filename="../orkamv_data_api_plugin.py" line="392"/>
+        <source>The processing timed out. Please try again with a smaller area.</source>
+        <translation>Eine Zeitüberschreitung beim Prozessieren ist eufgetreten. Bitte versuchen Sie es mit einem kleineren Gebiet noch einmal.</translation>
+    </message>
+    <message>
+        <location filename="../orkamv_data_api_plugin.py" line="394"/>
+        <source>The chosen bounding box is too big.</source>
+        <translation>Der gewählte Kartenausschnitt ist zu groß.</translation>
+    </message>
+    <message>
+        <location filename="../orkamv_data_api_plugin.py" line="396"/>
+        <source>Current job limit is reached. Please try again in a few minutes.</source>
+        <translation>Das Job Limit ist erreicht. Bitte versuchen Sie es in ein paar Minuten noch einmal.</translation>
+    </message>
+    <message>
+        <location filename="../orkamv_data_api_plugin.py" line="398"/>
+        <source>Unknown message type: {}: {}</source>
+        <translation>Unbekannter Nachrichtentyp: {}: {}</translation>
+    </message>
+</context>
+<context>
+    <name>OrkamvDataApiPluginDialogBase</name>
+    <message>
+        <location filename="../orkamv_data_api_plugin_dialog_base.ui" line="148"/>
+        <source>ORKa.MV Data API</source>
+        <translation>ORKa.MV Data API</translation>
+    </message>
+    <message>
+        <location filename="../orkamv_data_api_plugin_dialog_base.ui" line="34"/>
+        <source>Server URL:</source>
+        <translation>Server URL:</translation>
+    </message>
+    <message>
+        <location filename="../orkamv_data_api_plugin_dialog_base.ui" line="48"/>
+        <source>Map extent:</source>
+        <translation>Kartenausschnitt:</translation>
+    </message>
+    <message>
+        <location filename="../orkamv_data_api_plugin_dialog_base.ui" line="58"/>
+        <source>SVG folder:</source>
+        <translation>SVG Verzeichnis:</translation>
+    </message>
+    <message>
+        <location filename="../orkamv_data_api_plugin_dialog_base.ui" line="73"/>
+        <source>The icons needed for display are integrated into the chosen SVG directory.</source>
+        <translation>Die für die Darstellung benötigten Icons werden in das angegebene SVGVerzeichnis integriert.</translation>
+    </message>
+    <message>
+        <location filename="../orkamv_data_api_plugin_dialog_base.ui" line="80"/>
+        <source>Saving:</source>
+        <translation>Speicherung:</translation>
+    </message>
+    <message>
+        <location filename="../orkamv_data_api_plugin_dialog_base.ui" line="89"/>
+        <source>Temporary</source>
+        <translation>Temporär</translation>
+    </message>
+    <message>
+        <location filename="../orkamv_data_api_plugin_dialog_base.ui" line="99"/>
+        <source>Directory</source>
+        <translation>Verzeichnis</translation>
+    </message>
+    <message>
+        <location filename="../orkamv_data_api_plugin_dialog_base.ui" line="108"/>
+        <source>Target directory:</source>
+        <translation>Zielverzeichnis:</translation>
+    </message>
+    <message>
+        <location filename="../orkamv_data_api_plugin_dialog_base.ui" line="127"/>
+        <source>All needed files will be stored in the target directory.</source>
+        <translation>Im Zielverzeichnis werden alle benötigten Daten abgelegt.</translation>
+    </message>
+    <message>
+        <location filename="../orkamv_data_api_plugin_dialog_base.ui" line="189"/>
+        <source>Start</source>
+        <translation>Start</translation>
+    </message>
+</context>
+</TS>
